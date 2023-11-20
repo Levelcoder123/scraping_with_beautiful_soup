81b9c2c4c6b5564a8f76d57761650614d1d05779
Levelcoder123 <muaz.abdulwahid@barq.dev>
2023-11-20 17:34:11 +0500

    refine all code

--- scraping_via_selenium_and_soup.py
+++ scraping_via_selenium_and_soup.py
@@ -8,66 +8,61 @@ page_url = '/used-cars/search/-/ct_lahore/ca_college-road/'
 
 
 # load website using chrome webdriver
-def get_driver(website):
-    service = Service('../chromedriver')  # chromedriver path
+def get_driver():
+    service = Service('../../chromedriver')  # chromedriver path
     driver = webdriver.Chrome(service=service)
-    driver.get(website)
 
     return driver
 
 
-def get_soup(driver_for_drive):
+def get_soup_by_selenium_driver(website):
+    driver = get_driver()
+    driver.get(website)
+
     # get html from driver and make it soup
-    html_data = driver_for_drive.page_source
+    html_data = driver.page_source
     soup = BeautifulSoup(html_data, features="html.parser")
 
     return soup
 
 
-def get_product_details(delicious_soup):
+def get_product_details(soup):
     # get product details
-    car_name_data = delicious_soup.find('div', id='scroll_car_info').get_text(separator='\n', strip=True)
-    car_name = car_name_data.split('\n')[0]
+    name_data = soup.find('div', id='scroll_car_info').get_text(separator='\n', strip=True)
+    name = name_data.split('\n')[0]
 
-    price = delicious_soup.find('strong', class_='generic-green').get_text(separator=' ', strip=True)
+    price = soup.find('div', class_='price-box').get_text(separator=' ', strip=True)
 
-    city = delicious_soup.find('p', class_='detail-sub-heading').get_text(separator=' ', strip=True).split()[2]
-
-    location_data = (delicious_soup.find('p', class_='detail-sub-heading')
+    location_data = (soup.find('p', class_='detail-sub-heading')
                      .get_text(separator=',', strip=True).split(',', maxsplit=2))
     location = ','.join(location_data[:2])
 
     model_year = \
-        delicious_soup.find('table', class_='table table-bordered text-center table-engine-detail fs16').get_text(
-            separator=',', strip=True).split(',')[0]
+        soup.find('span', class_='engine-icon year').parent.get_text(strip=True)
 
-    mileage = delicious_soup.find('table', class_='table table-bordered text-center table-engine-detail fs16').get_text(
-        separator='-', strip=True).split('-')[1]
+    mileage = soup.find('span', class_='engine-icon millage').parent.get_text(strip=True)
 
-    seller_name_index = 0
-    seller_name_data = delicious_soup.find('div', class_='owner-detail-main').get_text(separator=',', strip=True).split(
+    seller_name_data = soup.find('div', class_='owner-detail-main').get_text(separator=',', strip=True).split(
         ',')
 
     # checking "is seller 'Dealer' or not? "
-    if seller_name_data[seller_name_index] == 'Dealer:':
-        seller_name_index = 1
-    seller_name = seller_name_data[seller_name_index]
+    seller_name = seller_name_data[0]
+    if seller_name == 'Dealer:':
+        seller_name = seller_name_data[1]
 
-    is_featured = False
-    featured_or_not = delicious_soup.find('div', id='myCarousel').get_text(separator=',', strip=True).split(',')[0]
-    if featured_or_not == 'FEATURED':
-        is_featured = True
+    featured_or_not = soup.find('div', id='myCarousel').get_text(separator=',', strip=True).split(',')[0]
 
-    images = delicious_soup.find('ul', class_='lSPager lSGallery').find_all('img')
+    is_featured = (True if featured_or_not == 'FEATURED' else False)
+
+    images = soup.find('div', id='myCarousel').find('ul', class_='lSPager lSGallery').findAll('img')
+
     cover_image_url = images[0]['data-original']
-
-    image_1_url = images[1]['data-original']
+    image_1_url = images[1]['data-original'] if len(images) > 1 else None
     image_2_url = images[2]['data-original'] if len(images) > 2 else None
 
     return {
-        'car_name': car_name,
+        'name': name,
         'price': price,
-        'city': city,
         'location': location,
         'model_year': model_year,
         'mileage': mileage,
@@ -75,53 +70,46 @@ def get_product_details(delicious_soup):
         'is_featured': is_featured,
         'cover_image_url': cover_image_url,
         'image_1': image_1_url,
-        'image_2': image_2_url
+        'image_2': image_2_url,
     }
 
 
-def get_page_urls():
-    page_no = 1
+def get_page_urls(last_page):
     page_urls_list = []
 
-    while True:
-        page_urls_list.append(f'{base_url}{page_url}?page={page_no}')
-
-        if page_no == 3:
-            break
-        page_no += 1
+    for page_num in range(1, len(last_page)):
+        page_urls_list.append(f'{base_url}{page_url}?page={page_num}')
 
     return page_urls_list
 
 
-def get_products_url(page_urls):
-    product_urls_list = []
+def get_products_urls(page_urls):
+    products_urls_list = []
 
-    for page in page_urls:
-        ready_driver = get_driver(page)
-        soup_for_taste = get_soup(ready_driver)
-        all_product_urls = soup_for_taste.find_all('a', class_='car-name ad-detail-path')
+    for url in page_urls:
+        soup = get_soup_by_selenium_driver(url)
+        all_product_urls = soup.find_all('a', class_='car-name ad-detail-path')
 
         for product_url in all_product_urls:
             product_href = product_url['href']
-            product_urls_list.append(base_url + product_href)
+            products_urls_list.append(base_url + product_href)
 
-    return product_urls_list
+    return products_urls_list
 
 
-def get_all_products_details(product_urls_list):
-    details_of_all_products = []
+def get_products_details(product_urls_list):
+    products_details = []
 
     for product_url in product_urls_list:
-        driver_for_ride = get_driver(product_url)
-        tasty_soup = get_soup(driver_for_ride)
-        details_of_all_products.append(get_product_details(tasty_soup))
+        soup = get_soup_by_selenium_driver(product_url)
+        products_details.append(get_product_details(soup))
 
-    return details_of_all_products
+    return products_details
 
 
-pages_url = get_page_urls()
-all_products_url = get_products_url(pages_url)
-products_details = get_all_products_details(all_products_url)
+pages_urls = get_page_urls(last_page='')
+products_urls = get_products_urls(pages_urls)
+products_all_details = get_products_details(products_urls)
 
-df = pd.DataFrame(products_details)
+df = pd.DataFrame(products_all_details)
 df.to_excel('all_products_details.xlsx', index=False)
