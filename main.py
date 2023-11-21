b0c7e02eaf783dd6800d9de4b7e652c8fcae201d
Levelcoder123 <muaz.abdulwahid@barq.dev>
2023-11-21 17:01:54 +0500

    final !!!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scraper import get_page_urls, get_product_urls, get_all_product_details
 
 latest_date_time = datetime.now()
-formatted_datetime = latest_date_time.strftime("%Y-%m-%d %H:%M")
+formatted_datetime = latest_date_time.strftime("%Y_%m_%d_%H_%M")
 
 page_url = '/used-cars/search/-/ct_lahore/ca_college-road/'
 page_urls = get_page_urls(page_url)
@@ -13,4 +13,4 @@ product_urls = get_product_urls(page_urls)
 product_all_details = get_all_product_details(product_urls)
 
 df = pd.DataFrame(product_all_details)
-df.to_excel(f'{formatted_datetime}.xlsx', index=False)
+df.to_excel(f'pakwheels_data_{formatted_datetime}.xlsx', index=False)
